ed6bef7e32e1db2c8b0c9ec503eae9dfb3ec7703
Annaya98 <adavis106@student.sierracollege.edu>
2025-11-05 17:58:45 -0800

    Initial commit

---
+++ crack.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../md5.h"
+
+#if __has_include("fileutil.h")
+#include "fileutil.h"
+#endif
+
+#define PASS_LEN 50     // Maximum length any password will be.
+#define HASH_LEN 33     // Length of hash plus one for null.
+
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3) 
+    {
+        printf("Usage: %s hash_file dictionary_file\n", argv[0]);
+        exit(1);
+    }
+
+    // TODO: Read the hashes file into an array.
+    //   Use either a 2D array or an array of arrays.
+    //   Use the loadFile function from fileutil.c
+    //   Uncomment the appropriate statement.
+    int size;
+    //char (*hashes)[HASH_LEN] = loadFile(argv[1], &size);
+    //char **hashes = loadFile(argv[1], &size);
+    
+    // CHALLENGE1: Sort the hashes using qsort.
+    
+    // TODO
+    // Open the password file for reading.
+
+    // TODO
+    // For each password, hash it, then use the array search
+    // function from fileutil.h to find the hash.
+    // If you find it, display the password and the hash.
+    // Keep track of how many hashes were found.
+    // CHALLENGE1: Use binary search instead of linear search.
+
+    // TODO
+    // When done with the file:
+    //   Close the file
+    //   Display the number of hashes found.
+    //   Free up memory.
+}
